8924f92abd9e8d84dc6aec3f4bfb44c693e3037e
rohit-0308 <rohitkumar860311@gmail.com>
2021-03-23 22:28:01 +0530

    load decode JSON file

--- lib/models/products.dart
+++ lib/models/products.dart
@@ -6,8 +6,7 @@ class CatalogModel {
       desc: "Apple iPhone 12th Generation",
       price: 120000,
       color: '#35505A',
-      image:
-          'https://www.google.com/imgres?imgurl=https%3A%2F%2Fstore.storeimages.cdn-apple.com%2F4982%2Fas-images.apple.com%2Fis%2Fiphone-12-blue-select-2020%3Fwid%3D940%26hei%3D1112%26fmt%3Dpng-alpha%26qlt%3D80%26.v%3D1604343704000&imgrefurl=https%3A%2F%2Fwww.apple.com%2Fshop%2Fbuy-iphone%2Fiphone-12%2F6.1-inch-display-64gb-blue-unlocked&tbnid=c8o3P3ld8pXOPM&vet=12ahUKEwiIudHdrL7vAhVUXX0KHQe4CecQMygBegUIARCGAg..i&docid=JTLWolBu0o9wcM&w=940&h=1112&q=iphone%2012&ved=2ahUKEwiIudHdrL7vAhVUXX0KHQe4CecQMygBegUIARCGAg',
+      image: "",
     ),
     Item(
       id: 2,
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,24 +1,55 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_app_learning/models/products.dart';
 import 'package:flutter_app_learning/widgets/drawer.dart';
 import 'package:flutter_app_learning/widgets/product_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  loadData() async {
+    final productJson =
+        await rootBundle.loadString("assets/files/products.json");
+    final decodedData = jsonDecode(productJson);
+    var productsData = decodedData["products"];
+    print(productsData);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: MyDrawer(),
-      appBar: AppBar(
-        title: Text("Home Page"),
-      ),
-      body: ListView.builder(
-        itemCount: CatalogModel.items.length,
-        itemBuilder: (context, index) {
-          return ProductWidget(
-            item: CatalogModel.items[index],
-          );
-        },
-      ),
+    final dummyList = List.generate(
+      20,
+      (index) => CatalogModel.items[0],
     );
+    return Scaffold(
+        drawer: MyDrawer(),
+        appBar: AppBar(
+          title: Text("Home Page"),
+        ),
+        body: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ProductWidget(
+              item: dummyList[0],
+            );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(
+            CupertinoIcons.cart,
+          ),
+          onPressed: () {},
+        ));
   }
 }
